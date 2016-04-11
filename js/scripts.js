0fbf7ea359a3fdc3baa7c88cbe43f54213aa907a
john dow and kevin deganos <john@>
2016-04-11 11:44:07 -0700

    created basic generator page with working form inputs in javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
 //Business Logic
 //----------------------------------------
-
+function Character(characterName, age, realm, race, profession){
+  this.name = characterName;
+  this.age = age;
+  this.realm = realm;
+  this.race = race;
+  this.profession = profession;
+  this.description;
+  this.quest;
+}
 
 //User Interface Logic
 //----------------------------------------
 $(document).ready(function() {
+  $("#submitCharacter").submit(function(event){
+    event.preventDefault();
 
+    var characterName = $('#characterName').val();
+    var newCharacter = new Character($('#characterName').val(), $('#age').val(), $('#realm').val(), $('#race').val(), $('#profession').val())
+    console.log(newCharacter);
+  });
 
 });
 
